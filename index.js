844e6789b4a7d5db3ab7b114c1496d6ebea90c75
Ryan Carlos <ryancarlos925@gmail.com>
2022-04-14 15:33:54 -0700

    commit tree b36c83f0aba6046c9416744ec049a5cedf3f867d

---
+++ index.js
@@ -0,0 +1,12 @@
+function receivesAFunction(callback) {
+    callback();
+}
+function returnsANamedFunction(){
+    return abc
+}
+
+function abc(){}
+
+function returnsAnAnonymousFunction() {
+    return (function() {}
+    )}
